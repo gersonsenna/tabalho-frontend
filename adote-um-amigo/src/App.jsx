f8bddc14672d06d9e7341b8c7936b4fc1f7bacd4
gerson <gersonsennaa@gmail.com>
2026-05-03 12:34:36 -0300

    refactor: extraindo componente Navbar para arquivo separado #2

--- adote-um-amigo/src/App.jsx
+++ adote-um-amigo/src/App.jsx
@@ -1,21 +1,15 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Cadastro from "./pages/Cadastro";
 import Listagem from "./pages/Listagem";
+import Navbar from "./components/Navbar"; // Importando o novo componente
 
 function App() {
   return (
     <BrowserRouter>
-      {/* Navbar com Tailwind */}
-      <nav className="bg-sky-700 p-4 text-white shadow-md">
-        <div className="container mx-auto flex gap-6">
-          <Link to="/" className="hover:text-orange-200 transition font-bold">Início</Link>
-          <Link to="/cadastro" className="hover:text-orange-200 transition font-bold">Cadastro</Link>
-          <Link to="/listagem" className="hover:text-orange-200 transition font-bold">Listagem</Link>
-        </div>
-      </nav>
+      {/* Chamamos o componente Navbar aqui */}
+      <Navbar />
 
-      {/* Área onde as páginas aparecem */}
       <main className="container mx-auto mt-6">
         <Routes>
           <Route path="/" element={<Home />} />
